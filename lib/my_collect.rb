318b9509916a1df0be3d2e6fab3a300f4ae710f0
ruthnewman <ruthnewman@gmail.com>
2018-12-02 18:24:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+  # code here
+  new_collection = []
+  counter = 0
+  while counter < collection.length 
+   yield collection[counter].capitalize
+   counter += 1
+  end 
+  new_collection
+end
